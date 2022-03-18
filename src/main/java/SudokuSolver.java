d5623ace8c90c4d549ac051b32ba8949f2e168e9
jimmychanshiji <jimmyfungchan@gmail.com>
2022-03-18 03:05:17 +0000

    SudokuKillerSolver

--- src/main/java/SudokuSolver.java
+++ src/main/java/SudokuSolver.java
@@ -1,42 +1,78 @@
 import javafx.scene.text.HitInfo;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
 public class SudokuSolver {
-    public void solveSudoku(char[][] board) {
+    public static void main(String[] args){
+        char[][] input = new char[][]{
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'}
+        };
+        char[][] rule = new char[][]{
+                {'a','a','e','j','j','p','p','t','v'},
+                {'a','e','e','e','k','k','p','t','v'},
+                {'b','e','e','k','k','q','p','p','v'},
+                {'b','c','h','h','h','q','q','s','s'},
+                {'c','c','c','l','l','l','s','s','s'},
+                {'c','c','i','i','n','n','n','s','w'},
+                {'d','f','f','i','m','m','r','r','w'},
+                {'d','g','f','m','m','r','r','r','u'},
+                {'d','g','f','f','o','o','r','u','u'}
+        };
+        HashMap<Character, Integer> ruleMap = new HashMap<>();
+        ruleMap.put('a',13);ruleMap.put('b',17);ruleMap.put('c',24);
+        ruleMap.put('d',12);ruleMap.put('e',29);ruleMap.put('f',32);
+        ruleMap.put('g',8);ruleMap.put('h',13);ruleMap.put('i',11);
+        ruleMap.put('j',12);ruleMap.put('k',18);ruleMap.put('l',24);
+        ruleMap.put('m',13);ruleMap.put('n',16);ruleMap.put('o',14);
+        ruleMap.put('p',29);ruleMap.put('q',12);ruleMap.put('r',31);
+        ruleMap.put('s',29);ruleMap.put('t',12);ruleMap.put('u',15);
+        ruleMap.put('v',12);ruleMap.put('w',9);
+        solveSudoku(input, rule, ruleMap);
+    }
+
+    public static void solveSudoku(char[][] board, char[][] rule, HashMap<Character, Integer> ruleMap) {
         if(board == null) return;
-        solver(board);
+        dfs(board, 0, rule, ruleMap);
     }
 
-    public static boolean solver(char[][] nums){
-        for(int i = 0; i < nums.length; i++){
-            for (int j = 0; j <  nums.length; j++) {
-                if(nums[i][j] == '.'){
-                    for (char k = '1'; k <= '9' ; k++) {
-                        if(validate(nums, i, j, k)){
-                            nums[i][j] = k;
-                            if(solver(nums))
-                                return true;
-                            else
-                                nums[i][j] = '.';
-                        }
-                    }
-                    return false;
-                }
+    private static boolean dfs(char[][] board, int d, char[][] rule, HashMap<Character, Integer> ruleMap) {
+        if (d==81) return true; //found solution
+        int i=d/9, j=d%9;
+        if (board[i][j]!='.') return dfs(board,d+1, rule, ruleMap);//prefill number skip
+
+        boolean[] flag=new boolean[10];
+        validate(board,i,j,flag, rule, ruleMap);
+        for (int k=1; k<=9; k++) {
+            if (flag[k]) {
+                board[i][j]=(char)('0'+k);
+                if(ruleMap.get(rule[i][j])<k)
+                    break;
+                ruleMap.put(rule[i][j], ruleMap.get(rule[i][j])-k);
+                if (dfs(board,d+1, rule, ruleMap)) return true;
+                ruleMap.put(rule[i][j], ruleMap.get(rule[i][j])+k);
             }
         }
-        return true;
+        board[i][j]='.'; //if can not solve, in the wrong path, change back to '.' and out
+        return false;
     }
-
-    public static boolean validate(char[][] nums, int x, int y, char value){
-        for(int i = 0; i < nums.length; i++){
-            for (int j = 0; j <  nums.length; j++) {
-                if((x == i || y == j || squareGroup(x,y) == squareGroup(i,j)) && nums[i][j] == value)
-                    return false;
-            }
+    private static void validate(char[][] board, int i, int j, boolean[] flag, char[][] rule, HashMap<Character, Integer> ruleMap) {
+        Arrays.fill(flag,true);
+        for (int k=0; k<9; k++) {
+            if (board[i][k]!='.') flag[board[i][k]-'0']=false;
+            if (board[k][j]!='.') flag[board[k][j]-'0']=false;
+            int r=i/3*3+k/3;
+            int c=j/3*3+k%3;
+            if (board[r][c]!='.') flag[board[r][c]-'0']=false;
         }
-        return true;
-    }
-
-    public static int squareGroup(int x, int y){
-        return x/3+y/3*3;
     }
 }
--- src/test/java/SudokuSolverTest.java
+++ src/test/java/SudokuSolverTest.java
@@ -1,33 +1,80 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class SudokuSolverTest {
+
     @Test
     public void sukudoSolver() {
         var leetCode = new SudokuSolver();
         char[][] input = new char[][]{
-                {'5','3','.','.','7','.','.','.','.'},
-                {'6','.','.','1','9','5','.','.','.'},
-                {'.','9','8','.','.','.','.','6','.'},
-                {'8','.','.','.','6','.','.','.','3'},
-                {'4','.','.','8','.','3','.','.','1'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','6','.','.','.','.','2','8','.'},
-                {'.','.','.','4','1','9','.','.','5'},
-                {'.','.','.','.','8','.','.','7','9'}
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'},
+                {'.','.','.','.','.','.','.','.','.'}
         };
+        char[][] rule = new char[][]{
+                {'a','a','e','j','j','p','p','t','v'},
+                {'a','e','e','e','k','k','p','t','v'},
+                {'b','e','e','k','k','q','p','p','v'},
+                {'b','c','h','h','h','q','q','s','s'},
+                {'c','c','c','l','l','l','s','s','s'},
+                {'c','c','i','i','n','n','n','s','w'},
+                {'d','f','f','i','m','m','r','r','w'},
+                {'d','g','f','m','m','r','r','r','u'},
+                {'d','g','f','f','o','o','r','u','u'}
+        };
+        HashMap<Character, Integer> ruleMap = new HashMap<>();
+        ruleMap.put('a',13);ruleMap.put('b',17);ruleMap.put('c',24);
+        ruleMap.put('d',12);ruleMap.put('e',29);ruleMap.put('f',32);
+        ruleMap.put('g',8);ruleMap.put('h',13);ruleMap.put('i',11);
+        ruleMap.put('j',12);ruleMap.put('k',18);ruleMap.put('l',24);
+        ruleMap.put('m',13);ruleMap.put('n',16);ruleMap.put('o',14);
+        ruleMap.put('p',29);ruleMap.put('q',12);ruleMap.put('r',31);
+        ruleMap.put('s',29);ruleMap.put('t',12);ruleMap.put('u',15);
+        ruleMap.put('v',12);ruleMap.put('w',9);
+
         char[][] expected = new char[][]{
-                {'5','3','4','6','7','8','9','1','2'},
-                {'6','7','2','1','9','5','3','4','8'},
-                {'1','9','8','3','4','2','5','6','7'},
-                {'8','5','9','7','6','1','4','2','3'},
-                {'4','2','6','8','5','3','7','9','1'},
-                {'7','1','3','9','2','4','8','5','6'},
-                {'9','6','1','5','3','7','2','8','4'},
-                {'2','8','7','4','1','9','6','3','5'},
-                {'3','4','5','2','8','6','1','7','9'}
+                {'5','6','7','9','3','8','1','4','2'},
+                {'2','4','1','6','7','5','9','8','3'},
+                {'9','8','3','4','2','1','5','6','7'},
+                {'8','2','9','3','1','4','7','5','6'},
+                {'6','1','5','8','9','7','2','3','4'},
+                {'3','7','4','5','6','2','8','9','1'},
+                {'1','9','6','2','5','3','4','7','8'},
+                {'7','3','8','1','4','9','6','2','5'},
+                {'4','5','2','7','8','6','3','1','9'}
         };
-        leetCode.solveSudoku(input);
+        leetCode.solveSudoku(input, rule, ruleMap);
+        Assertions.assertArrayEquals(expected,input);
+    }
+
+    @Test
+    public void sukudoSolver1() {
+        var leetCode = new SudokuSolver();
+        char[][] input = new char[][]{
+                {'.','.'},
+                {'.','.'}
+        };
+        char[][] rule = new char[][]{
+                {'a','a'},
+                {'b','b'}
+        };
+        HashMap<Character, Integer> ruleMap = new HashMap<>();
+        ruleMap.put('a',3);
+        ruleMap.put('b',7);
+
+        char[][] expected = new char[][]{
+                {'1','2'},
+                {'3','4'}
+        };
+        leetCode.solveSudoku(input, rule, ruleMap);
         Assertions.assertArrayEquals(expected,input);
     }
 }
